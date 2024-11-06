645f24c84c99ac9d9eb06ae4fbf66f7edb6e3215
MatthewTheProgrammmer <ozueiro514@gmail.com>
2024-11-05 23:19:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function abrirmenu(){
+    let menu = document.getElementById("menu")
+    if
+    (menu.style.display === "block"){
+        menu.style.display = "none";
+    } else{ menu.style.display = "block"}
+    }
+
+
+function abrirmenu2(){
+    let menu2 = document.getElementById("menu2")
+    if
+    (menu2.style.display === "block"){
+        menu2.style.display = "none";
+    } else{ menu2.style.display = "block"}
+    }
+    
+
+    function abrirmenu3(){
+        let menu3 = document.getElementById("menu3")
+        if
+        (menu3.style.display === "block"){
+            menu3.style.display = "none";
+        } else{ menu3.style.display = "block"}
+        }
+        
+        function alternarModoEscuro(){
+         document.body.classList.toggle("modo-escuro");}
+
+
+  
+
+         
